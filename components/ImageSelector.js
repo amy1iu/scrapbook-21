615c00950962765d1d39e17a1fbddae3fcef026a
Amy Liu <amyl20@lakesideschool.org>
2021-07-15 15:51:24 -0700

    07/15/21: added camera packages, camera screen opens, no functionality yet

---
+++ components/ImageSelector.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import Camera from 'expo-camera';
+import 
+
+const ImageSelector = props => {
+
+}
+
+export default ImageSelector;
